4d8d26c932a19c241fcd019fac0c0bdfe682b790
JV <jaiveer.chadda@gmail.com>
2026-09-23 23:29:15 +0400

    feat(parsing): Set up the basic regex initialisation and compilation functions and structs.

--- source/parsing/parse.c
+++ source/parsing/parse.c
@@ -1,4 +1,51 @@
 /// @file parsing/parse.c
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "parse.h"
 #include "types/types.h"
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
+
+#define memdup(src, size) memcpy(malloc((size)), (src), (size))
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
+
+struct rx__regex {
+	void **symbols;
+
+	const char *string;
+	uint64_t flags;
+};
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
+
+static inline regex_t rx_init(const char *const string, const uint64_t flags) {
+	const regex_t rx_obj = calloc(1, sizeof(struct rx__regex));
+	const size_t str_len = strlen(string) + 1;
+
+	*rx_obj = (struct rx__regex){
+		.symbols = malloc(str_len),
+		.string = memdup(string, str_len),
+		.flags = flags,
+	};
+
+	return rx_obj;
+}
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
+
+regex_t rx_compile(const char *const string, const uint64_t flags) {
+	const regex_t rx_obj = rx_init(string, flags);
+
+	for (const char *chr = rx_obj->string; *chr != '\0'; chr++) {
+		putchar(*chr);
+	}
+
+	putchar('\n');
+	return rx_obj;
+}
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
--- source/parsing/parse.h
+++ source/parsing/parse.h
@@ -3,5 +3,8 @@
 #ifndef PARSE_H_
 #define PARSE_H_
 
+typedef struct rx__regex *regex_t;
+
+regex_t rx_compile(const char *const string, const uint64_t flags);
 
 #endif /* !PARSE_H_ */
