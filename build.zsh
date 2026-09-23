cd397e1a413039e8960d7ad1908b38a21d82a4e7
JV <jaiveer.chadda@gmail.com>
2026-09-23 18:45:10 +0400

    feat(parsing): Define some macros to make it easier to create multi-character constants.

--- build.zsh
+++ build.zsh
@@ -21,7 +21,7 @@ function -- () {
   # all `-W...` warnings to enable
   local -a WARNINGS=( all extra pedantic vla )
   # all `-W-no-...` warnings to disable
-  local -ra NO_WARN=( )
+  local -ra NO_WARN=( multichar )
 
   WARNINGS+=( "no-${(@)^NO_WARN}" )
 
--- source/types/_tokens.h
+++ source/types/_tokens.h
@@ -3,12 +3,23 @@
 #ifndef TOKENS_H_
 #define TOKENS_H_
 
+#define num_1(a		 ) ((a) <<  0)
+#define num_2(a,b	 ) ((a) <<  8) | num_1(b)
+#define num_3(a,b,c	 ) ((a) << 16) | num_2(b,c)
+#define num_4(a,b,c,d) ((a) << 24) | num_3(b,c,d)
+
+#define num_dispatch(_1, _2, _3, _4, NAME, ...) NAME
+#define num(...) num_dispatch(__VA_ARGS__, num_4, num_3, num_2, num_1)(__VA_ARGS__)
+
 /**
  * @enum RxToken
  * @brief An enum holding every regex token to match against.
  */
 enum RxToken {
-	RXT
+	RXT_1 = num('1'),
+	RXT_2 = num('1', '2'),
+	RXT_3 = num('1', '2', '3'),
+	RXT_4 = num('1', '2', '3', '4'),
 };
 
 #endif /* !TOKENS_H_ */
