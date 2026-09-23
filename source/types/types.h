0a2dde882ef4cab565d9e1266981731749f404d6
JV <jaiveer.chadda@gmail.com>
2026-09-23 20:16:08 +0400

    feat(parsing): Remove the `_tokens.h` file - I don't think I need it atm tbh.

--- source/types/types.h
+++ source/types/types.h
@@ -5,15 +5,12 @@
 
 #include "_flags.h"
 #include "_chars.h"
-#include "_tokens.h"
 #include "_escapes.h"
 
 /** An enum holding all the flags that could be applied to a regex search. */
 typedef enum RxFlag	  RxFlag  ;
 /** An enum holding every semantically-significant regex character. */
 typedef enum RxChar	  RxChar  ;
-/** An enum holding every regex token to match against. */
-typedef enum RxToken  RxToken ;
 /** An enum holding every character that can appear after a backslash. */
 typedef enum RxEscape RxEscape;
 
