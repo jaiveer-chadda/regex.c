7b3a6a09ff529cd6439de2be65fca3d882812c5a
JV <jaiveer.chadda@gmail.com>
2026-09-23 15:49:04 +0400

    feat(parsing): Start over, this time defining what to parse with an enum, rather than having to do it manually.

--- source/main.c
+++ source/main.c
@@ -1,11 +1,9 @@
 /// @file main.c
 
 #include <stdio.h>
-#include <string.h>
+#include "types/types.h"
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-
-typedef char str[];
 
 const char LOREM[] = "Lorem ipsum dolor sit amet consectetur adipiscing elit, sed do eius modi tempora incididunt, "
 	"ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem "
@@ -15,43 +13,21 @@ const char LOREM[] = "Lorem ipsum dolor sit amet consectetur adipiscing elit, se
 
 /* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
 
-#define chrToStr(chr) ((str){ (chr), '\0' })
-
-/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-
-static inline int printMatch(const char *const match, const size_t len, const char *const col__) {
-	printf("\33[%sm%.*s%s", col__, (int)len, match, "\33[m");
-	fflush(stdout);
-	return (int)len;
-}
-
-/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
-
 int main(const int argc, const char *const argv[]) {
 	(void)argc, (void)argv;
 
-	const char pat1 = 'e';
-	const str  pat2 = "lo"; const size_t pat2len = sizeof(pat2) - 1;
+	const char *const string  = LOREM;
+	const char *const pattern = "e[ltd]";
 
-	for (const char *chr = LOREM; *chr != '\0'; chr++) {
-		if (strncmp(chr, pat2, pat2len) == 0) {
-			int len = printMatch(chr, pat2len, "33") - 1;
+	const char *pat_ptr = pattern;
+	(void)pat_ptr;
 
-			while (*chr != '\0' && len > 0) chr++, len--;
-			if (*chr == '\0') break;
-
-			continue;
-		}
-
-		if (*chr == pat1) {
-			printMatch(chrToStr(*chr), 1, "31");
-			continue;
-		}
-
-		putchar(*chr);
+	for (const char *str_ptr = string; *str_ptr != '\0'; str_ptr++) {
+		putchar(*str_ptr);
 	}
 
 	putchar('\n');
-
 	return 0;
 }
+
+/* ————————————————————————————————————————————————————————————————————————————————————————————————————————————————— */
