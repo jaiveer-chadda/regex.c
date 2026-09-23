0108bac7735833d4fa1615abe1aeb844b6818e3f
JV <jaiveer.chadda@gmail.com>
2026-09-23 16:11:54 +0400

    feat(parsing): Create the `RxFlag` enum, to hold all flags that I intend to implement.
    I basically just scoured the internet for any flags that have been implemented before, and added them all.

--- source/types/_flags.h
+++ source/types/_flags.h
@@ -4,7 +4,24 @@
 #define FLAGS_H_
 
 enum RxFlag {
-	RXF
+	RXF_GLOBAL		= 'g', RXF_g = 'g', /** Find all matches instead of just the first. */
+	RXF_MULTILINE	= 'm', RXF_m = 'm', /** Match `^` and `$` at line boundaries. */
+	RXF_IGNORECASE	= 'i', RXF_i = 'i', /** Ignore case. */
+
+	RXF_EXTENDED	= 'x', RXF_x = 'x', /** Spaces are ignored, and EoL comments are enabled. */
+	RXF_NAMEDONLY	= 'n', RXF_n = 'n', /** Named captures only. */
+	RXF_SINGLELINE	= 's', RXF_s = 's', /** The `.` wildcard matches `\n`. */
+	RXF_NOBACKTRACK	= 'N', RXF_N = 'N', /** Disable backtracking. */
+	RXF_RIGHTTOLEFT	= 'R', RXF_R = 'R', /** Match right to left. */
+
+	RXF_UNICODE		= 'u', RXF_u = 'u', /** Treat multibyte characters as a single character. */
+	RXF_DOLLAREND	= 'D', RXF_D = 'D', /** Match `$` only at string end. */
+	RXF_DUPGROUPS	= 'J', RXF_J = 'J', /** Allow duplicate group names. */
+
+	RXF_STICKY		= 'y', RXF_y = 'y', /** ? Perform a "sticky" search. */
+	RXF_ANCHOR		= 'A', RXF_A = 'A', /** ? Anchor at search start. */
+	RXF_LAZYQUANT	= 'U', RXF_U = 'U', /** ? Lazy quantifiers. */
+	RXF_NOCASEFOLD	= 'r', RXF_r = 'r', /** ? Restrict case folding. */
 };
 
 #endif /* !FLAGS_H_ */
