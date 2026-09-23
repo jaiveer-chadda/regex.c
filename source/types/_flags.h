da7ca55e4f56fcaa86b97ff34118624186e39667
JV <jaiveer.chadda@gmail.com>
2026-09-23 16:38:52 +0400

    refactor(parsing): Organise the flags into sections, and fix all of their descriptions.

--- source/types/_flags.h
+++ source/types/_flags.h
@@ -4,28 +4,43 @@
 #define FLAGS_H_
 
 enum RxFlag {
-	RXF_GLOBAL		= 'g', RXF_g = 'g', /** Find all matches instead of just the first. */
-	RXF_MULTILINE	= 'm', RXF_m = 'm', /** Match `^` and `$` at line boundaries. */
-	RXF_IGNORECASE	= 'i', RXF_i = 'i', /** Ignore case. */
 
-	RXF_EXTENDED	= 'x', RXF_x = 'x', /** Spaces are ignored, and EoL comments are enabled. */
-	RXF_NAMEDONLY	= 'n', RXF_n = 'n', /** Named captures only. */
-	RXF_SINGLELINE	= 's', RXF_s = 's', /** The `.` wildcard matches `\n`. */
-	RXF_NOBACKTRACK	= 'N', RXF_N = 'N', /** Disable backtracking. */
-	RXF_RIGHTTOLEFT	= 'R', RXF_R = 'R', /** Match right to left. */
+	/* —— General ——————————————————————————————————————————— */
 
-	RXF_UNICODE		= 'u', RXF_u = 'u', /** Treat multibyte characters as a single character. */
-	RXF_DOLLAREND	= 'D', RXF_D = 'D', /** Match `$` only at string end. */
-	RXF_DUPGROUPS	= 'J', RXF_J = 'J', /** Allow duplicate group names. */
+	RXF_GLOBAL		= 'g', /** Global				– Find all matches instead of just the first. */
+	RXF_EXTENDED	= 'x', /** Extended				– Spaces are ignored, and EoL comments are enabled. */
 
-	RXF_FIXEDSTR	= 'F', RXF_F = 'F', /** Treat the whole pattern as a literal. */
-	RXF_INVMATCH	= 'V', RXF_V = 'V', /** Return everything that doesn't match. */
-	RXF_WORDONLY	= 'w', RXF_w = 'w', /** Only match entire words. */
+	/* —— Regex Parsing ————————————————————————————————————— */
 
-	RXF_STICKY		= 'y', RXF_y = 'y', /** ? Perform a "sticky" search. */
-	RXF_ANCHOR		= 'A', RXF_A = 'A', /** ? Anchor at search start. */
-	RXF_LAZYQUANT	= 'U', RXF_U = 'U', /** ? Lazy quantifiers. */
-	RXF_NOCASEFOLD	= 'r', RXF_r = 'r', /** ? Restrict case folding. */
+	RXF_WORDONLY	= 'w', /** Words Only			– Only match entire words. */
+	RXF_FIXEDSTR	= 'F', /** Fixed Strings		– Treat the whole pattern as a literal. */
+	RXF_UNGREEDY	= 'U', /** Ungreedy	Quantifiers	– Quantifiers are lazy by default. `?` makes them greedy. */
+	RXF_MULTILINE	= 'm', /** Multiline Matching	– Match `^` and `$` at line boundaries. */
+	RXF_DOLLAREND	= 'D', /** Dollar Marks End		– Match `$` only at the absolute end of the string. */
+	RXF_SINGLELINE	= 's', /** Single Line			– The dot `.` widcard matches newlines `\n`. */
+
+	/* —— Input Parsing ————————————————————————————————————— */
+
+	RXF_UNICODE		= 'u', /** Unicode				– Treat multibyte characters as a single character. */
+	RXF_IGNORECASE	= 'i', /** Ignore Case			– Match case-insensitively. */
+
+	/* —— Searching Behaviour ——————————————————————————————— */
+
+	RXF_STICKY		= 'y', /** Sticky Search		– Perform a "sticky" search - all matches have to be sequential. */
+	RXF_ANCHOR		= 'A', /** Anchor at Start		– First match is at start of text, others have to be sequential. */
+	RXF_NOBACKTRACK	= 'N', /** No Backtracking		– Disable backtracking. */
+	RXF_RIGHTTOLEFT	= 'R', /** Right to Left		– Match right to left. */
+
+	/* —— Capturing Behaviour ——————————————————————————————— */
+
+	RXF_DUPGROUPS	= 'J', /** Duplicate Groups		– Allow duplicate group names. */
+	RXF_NONCAPTURE	= 'n', /** No Capture			– Groups are non-capturing by default. `(?:` makes them capture. */
+
+	/* —— Return Format ————————————————————————————————————— */
+
+	RXF_INVMATCH	= 'V', /** Invert Match			– Return everything that doesn't match. */
+
+	/* —————————————————————————————————————————————————————— */
 };
 
 #endif /* !FLAGS_H_ */
